0c849c89acbe49f81bd998b91347052121709089
Evan Olsen <evan.olsen@gmail.com>
2014-03-02 21:54:42 -0500

    Mobile Nav Update with Search!

--- js/main.js
+++ js/main.js
@@ -77,21 +77,32 @@ $(document).ready(function() {
 	$('.video-container').fitVids();
   $('#navbarsearch-button').click(function(e){
     e.stopPropagation();
-    if ($(window).width() > 768 && $(window).width() < 985){
+    if ($(window).width() < 985){
       $('#navbarsearch-button').css('background-color','#79a736');
     };
     $('.main-nav-search #input-wrap').css('opacity','100');
     $('#navbarsearch-input').focus();
-    $('#navbarsearch-input').animate({
-        opacity: 100
-    }, 200, function(){
-       $('#navbarsearch-input').css({width:'150px'});
-     });
+    if ($(window).width() < 500){
+      $('#navbarsearch-input').animate({
+          opacity: 100
+      }, 200, function(){
+         $('#navbarsearch-input').css({width:'300px'});
+       });
+    } else {
+      $('#navbarsearch-input').animate({
+          opacity: 100
+      }, 200, function(){
+         $('#navbarsearch-input').css({width:'150px'});
+       });
+    };
   });
   $(document).click(function() {
-      $('.main-nav-search #input-wrap').css('opacity','0');
-      $('#navbarsearch-input').css({width:'0px','opacity':'0'});
-      $('#navbarsearch-button').css('background-color','transparent');
+      if($(this.activeElement).is(':focus') != true){
+        console.log($(this).is(':focus'));
+        $('.main-nav-search #input-wrap').css('opacity','0');
+        $('#navbarsearch-input').css({width:'0px','opacity':'0'});
+        $('#navbarsearch-button').css('background-color','transparent');
+      };
   });
 });
 
